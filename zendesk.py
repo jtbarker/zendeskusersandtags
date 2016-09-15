60e420b6bcbc5a1adaba9c406adb58afa43d1a77
Jon Barker <jonathan.t.barker@gmail.com>
2016-09-15 13:50:05 -0700

    Add files via upload

---
+++ zendesk.py
@@ -0,0 +1,32 @@
+# Zenpy accepts an API token
+
+from zenpy import Zenpy
+
+creds = {
+    'email' : 'jonb@everstring.com',
+    'token' : '58xAJP6nqEH1FcQvPNeapvSskSUUQMDpmhkiru6U',
+    'subdomain': 'everstring'
+}
+
+
+# Default
+zenpy = Zenpy(**creds)
+
+# Alternatively you can provide your own requests.Session object
+# zenpy = Zenpy(**creds, session=some_session)
+
+tickets = []
+gravitron = {}
+for ticket in zenpy.tickets():
+    tickets.append(ticket.to_dict())
+    domain = (str(ticket.requester.email.split('@')[-1]))
+    tickets[-1]["domain"] = domain
+    if domain not in gravitron.keys():
+      gravitron[domain] = ticket.tags
+    else:
+      gravitron[domain] = list(set(gravitron[domain]+ticket.tags))
+print gravitron
+
+for k in gravitron.keys():
+  if "feature_request_deal_blocking" in gravitron[k]:
+      print k, 'feature failure' 
